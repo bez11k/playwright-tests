cb66773af8e36a55a9c2423b97eb6194284d9495
Maksim Pyzik <maksim.pyzik@instinctools.com>
2023-06-08 08:44:22 +0300

    lesson 11.2

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('My Account', () => {
-    test('Login', async ({ page }) => {
+    test.beforeEach( async ({ page }) => {
         await page.goto('/my-account');
         await page.locator('#username').fill('practiceuser1');
         await page.locator('#password').fill('PracticePass1!');
@@ -10,13 +10,13 @@ test.describe('My Account', () => {
     })
     
     test('Access Orders', async ({ page }) => {
-        await page.goto('/my-account');
+//        await page.goto('/my-account');
         await page.locator('li.woocommerce-MyAccount-navigation-link:nth-child(2) > a:nth-child(1)').click();
         await expect(page).toHaveURL(/.*orders/);
     });
 
     test('Access Downloads', async ({ page }) => {
-        await page.goto('/my-account');
+//        await page.goto('/my-account');
         await page.locator('li.woocommerce-MyAccount-navigation-link:nth-child(3) > a:nth-child(1)').click();
         await expect(page).toHaveURL(/.*downloads/);
     });
