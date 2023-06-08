83937e9261d40ea0a797bad2f351960c3001103d
Maksim Pyzik <maksim.pyzik@instinctools.com>
2023-06-08 08:58:07 +0300

    lesson 11.3

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,7 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test.describe('My Account', () => {
-    test.beforeEach( async ({ page }) => {
+test.describe.serial('My Account', () => {
+    let page: Page;
+
+    test.beforeAll( async ({browser}) => {
+        page = await browser.newPage();
         await page.goto('/my-account');
         await page.locator('#username').fill('practiceuser1');
         await page.locator('#password').fill('PracticePass1!');
@@ -9,13 +12,13 @@ test.describe('My Account', () => {
         await expect(page.locator('a:has-text("Logout")')).toBeVisible();
     })
     
-    test('Access Orders', async ({ page }) => {
+    test('Access Orders', async () => {
 //        await page.goto('/my-account');
         await page.locator('li.woocommerce-MyAccount-navigation-link:nth-child(2) > a:nth-child(1)').click();
         await expect(page).toHaveURL(/.*orders/);
     });
 
-    test('Access Downloads', async ({ page }) => {
+    test('Access Downloads', async () => {
 //        await page.goto('/my-account');
         await page.locator('li.woocommerce-MyAccount-navigation-link:nth-child(3) > a:nth-child(1)').click();
         await expect(page).toHaveURL(/.*downloads/);
